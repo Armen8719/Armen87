7f0d9342935b567c6fe6eca9754743380b873b33
Armen8719 <59921270+Armen8719@users.noreply.github.com>
2020-01-15 18:50:55 +0400

    Update bind.py

--- bind.py
+++ bind.py
@@ -2,6 +2,10 @@ from tkinter import *
 root=Tk()
 # def func1(event):
 #     print(event.x,event.y)
+def fe(event):
+    l4.config(bg='red', fg='black')
+def f2(event):
+    l4.config(bg='black', fg='black')
 def f1(event):
     exec(t.get(1.0,END))#get() funkcian entry u text meji gracn vercnuma
 def print(*args):
@@ -11,12 +15,17 @@ def print(*args):
 l1=Label(text='click 1', width=10, height=2)
 l2=Label(text='click 2', width=10, height=2)
 l3=Label(text=' ', width=100, height=5, anchor=W)
+l4=Label(text='Enteri funkcia', width=100, height=8, anchor=W)
+b=Button(text='Enter', width=20, height=3)
 t=Text()
 t.pack()
 l1.pack()
 l2.pack()
 l3.pack()
+l4.pack()
 root.bind('<Control-Return>', f1)
 # root.bind('<Key>', func1) #bind funkcian patasxanatua te knopkin sexmeluc inch pti ani
 #root.bind('<Button-1>', func1) #bind Button-1  mkan dzax klikna
+l4.bind('<Enter>', fe) 
+l4.bind('<Leave>', f2)
 root.mainloop()
